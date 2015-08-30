0cc3707ff52109322c94f311b0d6fde3e82eceff
David R. MacIver <david@drmaciver.com>
2015-08-30 11:02:02 +0100

    Tidy up and document

--- src/tracking.py
+++ src/tracking.py
@@ -1,5 +1,60 @@
+"""
+This module exports an API that is intended to support glass box testing and
+other tools that must introspect program state.
+
+It is deliberately quite a low level API. You will probably want to build
+better abstractions on top of it.
+
+It exposes only two functions: begin and collect. begin starts gathering labels
+and collect returns a set of string labels that indicate what has happened
+since the previous calls.
+
+Note: This is incompatible with running other tracing based APIs at the time.
+Any tracer that was previously running will be restored at the end of the last
+collect() call, but it will be suspended until then.
+"""
+
 import sys
 from array import array as arr
+
+
+__all__ = ['begin', 'collect']
+
+
+def begin():
+    """Start tracking program state.
+
+    If begin() has previously been called, any labels that occur during this
+    execution will also be made visible to previous begin calls.
+    """
+    global prev_state
+    prev_state = 0
+    push_array()
+    prev_tracers.append(sys.gettrace())
+    sys.settrace(tracer)
+
+
+def collect():
+    """Return a set of string labels corresponding to events that have been
+    seen since the last begin() call"""
+    restore = prev_tracers.pop()
+    sys.settrace(None)
+    result = set()
+    array_state = arrays.pop()
+    for a in arrays:
+        for i in range(len(array_state)):
+            a[i] += array_state[i]
+    for i in range(len(array_state)):
+        if array_state[i]:
+            for t, l in enumerate(levels, 1):
+                if array_state[i] <= l:
+                    result.add(label(i, t))
+                    break
+            else:
+                result.add(label(i, 1 + len(levels)))
+    sys.settrace(restore)
+    return result
+
 
 STATE_SIZE = 2 ** 16
 STATE_MASK = STATE_SIZE - 1
@@ -42,33 +97,5 @@ def tracer(frame, event, arg):
 levels = [1, 2, 3, 4, 8, 16, 32, 128]
 
 
-def begin():
-    global prev_state
-    prev_state = 0
-    push_array()
-    prev_tracers.append(sys.gettrace())
-    sys.settrace(tracer)
-
-
 def label(a, b):
     return "%d:%d" % (a, b)
-
-
-def collect():
-    restore = prev_tracers.pop()
-    sys.settrace(None)
-    result = set()
-    array_state = arrays.pop()
-    for a in arrays:
-        for i in range(len(array_state)):
-            a[i] += array_state[i]
-    for i in range(len(array_state)):
-        if array_state[i]:
-            for t, l in enumerate(levels, 1):
-                if array_state[i] <= l:
-                    result.add(label(i, t))
-                    break
-            else:
-                result.add(label(i, 1 + len(levels)))
-    sys.settrace(restore)
-    return result
